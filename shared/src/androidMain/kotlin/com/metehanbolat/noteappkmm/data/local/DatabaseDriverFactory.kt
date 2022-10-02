1f398871ae0a233ecbe34476d54e43726b0e3790
Metehan <metehan.bolat.ie@gmail.com>
2022-10-03 00:24:18 +0300

    * DatabaseDriverFactory was completed for all Platforms.

---
+++ shared/src/androidMain/kotlin/com/metehanbolat/noteappkmm/data/local/DatabaseDriverFactory.kt
@@ -0,0 +1,13 @@
+package com.metehanbolat.noteappkmm.data.local
+
+import android.content.Context
+import com.metehanbolat.noteappkmm.database.NoteDatabase
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import com.squareup.sqldelight.db.SqlDriver
+
+actual class DatabaseDriverFactory(private val context: Context) {
+    actual fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(NoteDatabase.Schema, context, "note.db")
+    }
+
+}
